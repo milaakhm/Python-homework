73b253aa25df22651f8f516be9f96ada00ccd009
milaakhm <mila.7373@mail.ru>
2023-09-10 18:35:30 +0300

    задание 7

---
+++ Homework  1/lesson_1_task_7.py
@@ -0,0 +1,45 @@
+def one():
+    print("1")
+#one()
+
+def two ():
+    print("2")
+#two()
+
+def three ():
+    print("3")
+#three()
+
+def four ():
+    print("4")
+#four()
+
+def five ():
+    print("5")
+#five()
+
+def six ():
+    print("6")
+#six()
+    
+def seven ():
+    print("7")
+#seven()
+
+def eight ():
+    print("8")
+#eight()
+
+def nine ():
+    print("9")
+#nine()
+
+def zero ():
+    print("0")
+#zero()
+    
+e=8 
+f=5
+t=3   
+z=0   
+print(e,e,z,z,f,f,f,t,f,t,f)
